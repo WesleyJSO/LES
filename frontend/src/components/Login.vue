8e8292bc8ad781953755b4714c8cd769dea8e806
Wesley Silva <wesley.silva@muralis.com.br>
2018-08-26 00:19:42 -0300

    Altera  front para se ajustar as alterações do backend, mock de acesso

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -5,8 +5,9 @@
       <v-toolbar dark>
         <v-toolbar-title>Login</v-toolbar-title>
       </v-toolbar>
-        <v-alert :color="msgCor" :value="isErro" v-text="msgErro" transition="scale-transition" />
-
+        <li v-for="error in msgErro" :key="error">
+          <v-alert :color="msgCor" :value="isErro" v-text="error" transition="scale-transition" />
+        </li>
       <v-form v-model="valido" @submit.prevent="findByNomeAndSenha">
         <v-card-text>
           <text v-bind="login" />
@@ -31,11 +32,12 @@ import axios from 'axios'
 export default {
   data: () => ({
     login: [],
+    usuario: [],
     valido: false,
     nome: '',
     senha: '',
     isErro: false,
-    msgErro: '',
+    msgErro: [],
     msgCor: '',
     nomeRegras: [v => !!v || 'Nome obrigatório!'],
     senhaRegras: [
@@ -44,34 +46,58 @@ export default {
     ]
   }),
   methods: {
-    async findByNomeAndSenha () {
+    findByNomeAndSenha () {
       if ((!this.nome && !this.senha) || this.senha < 3) {
         this.isErro = true
         this.msgCor = 'warning'
         if (!this.nome && !this.senha) {
-          this.msgErro = 'Nome e Senha devem ser preenchidos!'
+          this.msgErro = ['Nome e Senha devem ser preenchidos!']
         } else {
-          this.msgErro = 'Senha deve conter no mínimo 3 caracteres!'
+          this.msgErro = ['Senha deve conter no mínimo 3 caracteres!']
         }
       } else {
-        let login = {nomeLogin: this.nome, senha: this.senha}
-        axios.post('http://localhost:8080/login', login).then(response => {
-          if (response.data === '') {
-            this.isErro = true
-            this.msgCor = 'warning'
-            this.msgErro = 'Usuário não encontrado!'
-          } else {
-            this.login = response.data
+        let usuario = {
+          login: {
+            nomeLogin: this.nome,
+            senha: this.senha
+          }
+        }
+        axios.post('http://localhost:8080/usuarioLogin', usuario).then(response => {
+          let resultado = response.data
+          if (resultado.listaResultado.length !== 0) {
+            this.usuario = response.data
             this.isErro = true
             this.msgCor = 'info'
-            this.$emit('message', this.login)
-            this.msgErro = 'Bem vindo ' + login.nomeLogin + '.'
+            this.msgErro = [`Bem vindo ${this.usuario.login.nomeLgin}.`]
+            this.$emit('login', this.usuario.login)
+          } else if (resultado.mensagem) {
+            this.isErro = true
+            this.msgCor = 'warning'
+            this.msgErro = [...resultado.mensagem]
+          } else {
+            /* this.isErro = true
+            this.msgCor = 'warning'
+            this.msgErro = [`Usuário ${usuario.login.nomeLogin} não encontrado!`] */
+
+            /* USER MOCK */
+            this.usuario = {
+              login: {
+                nomeLogin: 'wesley',
+                senha: '123',
+                ativo: true,
+                dataCriacao: 'null'
+              },
+              roles: [
+                {nome: 'ADMIN'}
+              ]
+            }
+            this.$emit('emittedUsuario', this.usuario)
           }
         },
         response => {
           this.isErro = true
           this.msgCor = 'error'
-          this.msgErro = 'Erro durante execução do serviço!'
+          this.msgErro = ['Erro durante execução do serviço!']
           console.log(response.data)
         })
       }
@@ -80,6 +106,3 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
