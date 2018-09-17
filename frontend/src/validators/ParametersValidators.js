cf7160559bd41bdba157ba5aed01abfb581551e1
Bruno Holanda <br.holanda@gmail.com>
2018-09-17 17:06:30 -0300

    LES - 023 Implementa validações de campos

--- frontend/src/validators/ParametersValidators.js
+++ frontend/src/validators/ParametersValidators.js
@@ -1,17 +1,23 @@
 export default class ParametersValidators {
   overtimeRules (overtime) {
-    if (!overtime || overtime <= 0) {
-      return ['Informe o valor da hora extra!']
+    if (!overtime) {
+      return ['Informe o adicional da hora extra!']
+    } else if (overtime < 50) {
+      return ['O adicional da hora extra deve ser no minimo 50%']
     }
   }
   nightOvertimeRules (nightOvertime) {
     if (!nightOvertime || nightOvertime <= 0) {
-      return ['Informe o valor da hora extra noturna!']
+      return ['Informe o adicional da hora extra noturna!']
+    } else if (nightOvertime < 20) {
+      return ['O adicional noturno deve ser de no minimo 20%']
     }
   }
   weekEndOvertimeRules (weekEndOvertime) {
     if (!weekEndOvertime || weekEndOvertime <= 0) {
-      return ['Informe o valor da hora extra de domingos e feriados!']
+      return ['Informe o adicional da hora extra de domingos e feriados!']
+    } else if (weekEndOvertime < 100) {
+      return ['O adicional de domingos e feriados deve ser no minimo 100%']
     }
   }
   retroactiveAppointmentRules (retroactiveAppointment) {
@@ -27,6 +33,8 @@ export default class ParametersValidators {
   bankCompensationRules (bankCompensationRules) {
     if (!bankCompensationRules || bankCompensationRules <= 0) {
       return ['Informe o prazo para compensação do banco de horas!']
+    } else if (bankCompensationRules < 6) {
+      return ['O prazo para compensação do banco deve ser de no mínimo 6 meses']
     }
   }
   hoursLimitRules (type, notSetLimit, hoursLimit) {
