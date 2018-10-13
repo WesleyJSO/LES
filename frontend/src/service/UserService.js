582cccc81a03f54fc7c91bc30563d7ce9a1f4813
José Victor Zeller <josevictorzg@gmail.com>
2018-10-13 16:45:33 -0300

    Corrige modelo de Tipos de solicitação representados na view.

--- frontend/src/service/UserService.js
+++ frontend/src/service/UserService.js
@@ -18,12 +18,12 @@ export const PAGINATION = { sortBy: 'id' }
 export const APPOINTMENTS = { title: 'Apontamento de Horas', table: 'Lista de Apontamentos', addAppointments: 'Apontar Horas', types: ['Entrada', 'Almoço', 'Retorno Almoço', 'Saída', 'Saída Particular'] }
 export const REQUEST = { title: 'Solicitação Abono/Falta',
   types: [
-    /* {id: 1, description: 'Falta'},
-    {id: 2, description: 'Consulta Médica'},
-    {id: 3, description: 'Abono'},
-    {id: 4, description: 'Hora Extran'},
-    {id: 5, description: 'Banco de Horas'},
-    {id: 6, description: 'Remanejamento de Horas/Dias'} */
+    /*
+    {id: 1, description: 'Consulta Médica'},
+    {id: 2, description: 'Abono'},
+    {id: 3, description: 'Hora Extra'},
+    {id: 4, description: 'Banco de Horas'},
+    {id: 5, description: 'Remanejamento de Horas/Dias'} */
     'Falta', 'Consulta Médica', 'Abono', 'Hora Extra', 'Banco de Horas', 'Remanejamento de Dias'
   ]
 }
