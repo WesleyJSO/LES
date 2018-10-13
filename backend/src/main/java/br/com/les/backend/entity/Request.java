3213a6663b26f915a6a133d89b3b62c0279ad59f
José Victor Zeller <josevictorzg@gmail.com>
2018-10-13 16:38:21 -0300

    Exclui subtipos de solicitação e adiciona nova entidade que as unifica.

--- backend/src/main/java/br/com/les/backend/entity/Request.java
+++ backend/src/main/java/br/com/les/backend/entity/Request.java
@@ -1,7 +1,7 @@
 package br.com.les.backend.entity;
 
+import java.util.Date;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,16 +10,21 @@ import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import br.com.les.backend.utils.RequestStatus;
+import br.com.les.backend.utils.RequestType;
 
 @Entity
 @Inheritance( strategy=InheritanceType.TABLE_PER_CLASS )
-public abstract class Request extends DomainEntity {
+public class Request extends DomainEntity {
 
 	private static final long serialVersionUID = 9026354384897022658L;
 
 	private String description;
-	private boolean agreed;
+	private RequestStatus status;
+	private Date startDate;
+	private Date endDate;
+	private RequestType type;
+	private byte[] attachment;
 	
 	@ManyToOne( fetch=FetchType.LAZY )
 	@JoinColumn( name="employee_id" )
@@ -28,23 +33,12 @@ public abstract class Request extends DomainEntity {
 	@ManyToMany( cascade={ CascadeType.MERGE, CascadeType.PERSIST } )
 	private List< Employee > notificatedColaboratorsList;
 
-	@OneToOne( fetch=FetchType.LAZY )
-	private RequestType requestType;
-
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-
-	public boolean isAgreed() {
-		return agreed;
-	}
-
-	public void setAgreed(boolean agreed) {
-		this.agreed = agreed;
 	}
 
 	public User getEmployee() {
@@ -62,12 +56,44 @@ public abstract class Request extends DomainEntity {
 	public void setNotificatedColaboratorsList(List<Employee> notificatedColaboratorsList) {
 		this.notificatedColaboratorsList = notificatedColaboratorsList;
 	}
-
-	public RequestType getRequestType() {
-		return requestType;
+	
+	public Date getStartDate() {
+		return startDate;
 	}
 
-	public void setRequestType(RequestType requestType) {
-		this.requestType = requestType;
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
 	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public byte[] getAttachment() {
+		return attachment;
+	}
+
+	public void setAttachment(byte[] attachment) {
+		this.attachment = attachment;
+	}
+
+	public RequestType getType() {
+		return type;
+	}
+
+	public void setType(RequestType requestType) {
+		this.type = requestType;
+	}
+
+	public RequestStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(RequestStatus status) {
+		this.status = status;
+	}	
 }
