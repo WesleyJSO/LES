4f501acb7d2f02ab72c0c0e696e32ed847514f77
José Victor Zeller <josevictorzg@gmail.com>
2018-10-14 17:50:07 -0300

    Altera super classe para adequação aos novos padrões adotados.

--- backend/src/main/java/br/com/les/backend/controller/RequestController.java
+++ backend/src/main/java/br/com/les/backend/controller/RequestController.java
@@ -5,21 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.les.backend.entity.Parameter;
 import br.com.les.backend.entity.Request;
 import br.com.les.backend.utils.Actions;
 import br.com.les.backend.utils.Result;
 
 @CrossOrigin
 @RestController
-public class RequestController extends AbstractController {
+public class RequestController extends AbstractController<Request> {
 	
-	@PostMapping( value="/request" )
-	public Result saveRequest (@RequestBody Request resquest) {
-		return run(Actions.SAVE.getValue()).execute(resquest, Actions.SAVE.getValue());
-	}
-	
-	@GetMapping( value="/request" )
-	public Result findAll () {
-		return run ( Actions.FIND_ALL.getValue() ).execute(new Request() , Actions.FIND_ALL.getValue() );
+	public RequestController() {
+		super(Request.class);
 	}
 }
