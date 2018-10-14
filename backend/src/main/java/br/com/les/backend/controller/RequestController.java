997b5aa459be8cc01539771cebd08879c7f2e6a9
José Victor Zeller <josevictorzg@gmail.com>
2018-10-14 17:55:30 -0300

    Corrige conflito proveniente do merge com a develop.

--- backend/src/main/java/br/com/les/backend/controller/RequestController.java
+++ backend/src/main/java/br/com/les/backend/controller/RequestController.java
@@ -1,15 +1,9 @@
 package br.com.les.backend.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.les.backend.entity.Parameter;
 import br.com.les.backend.entity.Request;
-import br.com.les.backend.utils.Actions;
-import br.com.les.backend.utils.Result;
 
 @CrossOrigin
 @RestController
