6a72113998c8070c42d77d3f88500a1faeb55071
Bruno Holanda <br.holanda@gmail.com>
2018-10-15 21:51:03 -0300

    LES-030 Implementa edição de apontamento

---
+++ frontend/src/components/shared/AppointDialog.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-card>
+      <v-card-title
+      class="headline grey lighten-2"
+      primary-title
+      >
+      {{ type }}
+      </v-card-title>
+
+      <v-flex xs12 sm9 md6 lg6 xl4>
+        <v-text-field type="time"
+          v-model="editedValue"
+          prepend-icon="timer"
+          label="Hora">
+        </v-text-field>
+      </v-flex>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn
+          color="red"
+          flat
+          @click="appoint('close')">
+          Cancelar
+      </v-btn>
+      <v-btn
+          color="green"
+          flat
+          @click="appoint('appoint')">
+          Apontar
+      </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+  props: ['type', 'value'],
+  data: () => ({
+    dialog: false,
+    editedValue: ''
+  }),
+  created () {
+    this.editedValue = this.value
+  },
+  methods: {
+    appoint (action) {
+      if (action === 'close' || this.editedValue === '--:--') {
+        this.$emit('editAppoint', false)
+      } else {
+        this.$emit('editAppoint', this.editedValue)
+      }
+    },
+    close () {
+      this.$emit('closeIt')
+    }
+  }
+}
+</script>
--- frontend/src/components/user/Appoint.vue
+++ frontend/src/components/user/Appoint.vue
@@ -20,14 +20,102 @@
                     class="elevation-10 form-container" >
             <template slot="items" slot-scope="props">
               <tr>
-                <td class="text-xs-center" >{{ props.item.morningEntrance || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.morningOut || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.afternoonEntrance || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.afternoonOut || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.nightEntrance || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.nightOut || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.particularExit || empty }}</td>
-                <td class="text-xs-center" >{{ props.item.particularExitReturn || empty }}</td>
+                <td class="text-xs-center" >{{ props.item.morningEntrance || empty }} 
+                  <v-dialog v-model="headers[0].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.morningEntrance || empty"
+                      type="Entrada Manhã" @editAppoint="editAppointment($event, 0)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.morningOut || empty }}
+                  <v-dialog v-model="headers[1].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.morningOut || empty"
+                      type="Saída Manhã" @editAppoint="editAppointment($event, 1)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.afternoonEntrance || empty }}
+                  <v-dialog v-model="headers[2].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.afternoonEntrance || empty"
+                      type="Entrada Tarde" @editAppoint="editAppointment($event, 2)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.afternoonOut || empty }}
+                  <v-dialog v-model="headers[3].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.afternoonOut || empty"
+                      type="Saída Tarde" @editAppoint="editAppointment($event, 3)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.nightEntrance || empty }}
+                  <v-dialog v-model="headers[4].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.nightEntrance || empty"
+                      type="Entrada Noite" @editAppoint="editAppointment($event, 4)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.nightOut || empty }}
+                  <v-dialog v-model="headers[5].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.nightOut || empty"
+                      type="Saída Noite" @editAppoint="editAppointment($event, 5)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.particularExit || empty }}
+                  <v-dialog v-model="headers[6].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.particularExit || empty"
+                      type="Saída Partícular" @editAppoint="editAppointment($event, 6)"></AppointDialog>
+                  </v-dialog>
+                </td>
+                <td class="text-xs-center" >{{ props.item.particularExitReturn || empty }}
+                  <v-dialog v-model="headers[7].dialog" max-width="300px" max-height="300px">
+                    <v-icon small
+                      slot="activator"
+                      :key="props.item.index"
+                      light>
+                      edit
+                    </v-icon>
+                    <AppointDialog :value="props.item.particularExitReturn || empty"
+                      type="Retorno" @editAppoint="editAppointment($event, 7)"></AppointDialog>
+                  </v-dialog>
+                </td>
               </tr>
             </template>
           </v-data-table>
@@ -37,7 +125,7 @@
 								<AppointButton v-for="button in buttons" :key="button.name" 
                   :disable="button.disable" 
                   :buttonName="button.name" 
-                  @emitAppoint="mountAppointment($event)">
+                  @emitAppoint="appoint($event)">
                 </AppointButton>
 							</v-flex>
             </v-card>
@@ -50,6 +138,7 @@
 
 <script>
 import AppointButton from '@/components/shared/AppointButton.vue'
+import AppointDialog from '@/components/shared/AppointDialog.vue'
 
 export default {
   data: () => ({
@@ -58,6 +147,7 @@ export default {
     messageColor: '',
     empty: '--:--',
     today: '',
+    dialog: false,
     buttons: [
       { name: 'Entrada Manhã', disable: false },
       { name: 'Saída Manhã', disable: true },
@@ -68,21 +158,22 @@ export default {
       { name: 'Saída Partícular', disable: true }
     ],
     headers: [
-      { text: 'Entrada Manhã', align: 'center', sortable: false },
-      { text: 'Saída Manhã', align: 'center', sortable: false },
-      { text: 'Entrada Tarde', align: 'center', sortable: false },
-      { text: 'Saída Tarde', align: 'center', sortable: false },
-      { text: 'Entrada Noite', align: 'center', sortable: false },
-      { text: 'Saída Noite', align: 'center', sortable: false },
-      { text: 'Saída Partícular', align: 'center', sortable: false },
-      { text: 'Retorno', align: 'center', sortable: false }
+      { text: 'Entrada Manhã', align: 'center', sortable: false, dialog: false },
+      { text: 'Saída Manhã', align: 'center', sortable: false, dialog: false },
+      { text: 'Entrada Tarde', align: 'center', sortable: false, dialog: false },
+      { text: 'Saída Tarde', align: 'center', sortable: false, dialog: false },
+      { text: 'Entrada Noite', align: 'center', sortable: false, dialog: false },
+      { text: 'Saída Noite', align: 'center', sortable: false, dialog: false },
+      { text: 'Saída Partícular', align: 'center', sortable: false, dialog: false },
+      { text: 'Retorno', align: 'center', sortable: false, dialog: false }
     ],
     appointment: {},
     employee: {id: 1},
     appointments: []
   }),
   components: {
-    AppointButton
+    AppointButton,
+    AppointDialog
   },
   computed: {
   },
@@ -92,25 +183,39 @@ export default {
     this.callApi()
   },
   methods: {
-    mountAppointment (button) {
+    editAppointment (value, index) {
+      if (value !== false) {
+        this.mountAppointment(this.headers[index].text, value)
+      }
+      this.headers[index].dialog = false
+    },
+    appoint (button) {
       let date = new Date()
       let time = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
       time += (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
-      if (button === 'Entrada Manhã') {
+      if (button === 'Saída Partícular') {
+        if (this.appointment.particularExit) {
+          button = 'Retorno'
+        }
+      }
+      this.mountAppointment(button, time)
+    },
+    mountAppointment (field, time) {
+      if (field === 'Entrada Manhã') {
         this.appointment.morningEntrance = time
-      } else if (button === 'morningOut') {
+      } else if (field === 'Saída Manhã') {
         this.appointment.morningOut = time
-      } else if (button === 'afternoonEntrance') {
+      } else if (field === 'Entrada Tarde') {
         this.appointment.afternoonEntrance = time
-      } else if (button === 'afternoonOut') {
+      } else if (field === 'Saída Tarde') {
         if (!this.appointment.morningOut) {
           this.appointment.morningOut = '12:00'
           this.appointment.afternoonEntrance = '12:00'
         }
         this.appointment.afternoonOut = time
-      } else if (button === 'nightEntrance') {
+      } else if (field === 'Entrada Noite') {
         this.appointment.nightEntrance = time
-      } else if (button === 'nightOut') {
+      } else if (field === 'Saída Noite') {
         if (!this.appointment.morningOut && this.appointment.morningEntrance) {
           this.appointment.morningOut = '12:00'
           this.appointment.afternoonEntrance = '12:00'
@@ -120,12 +225,10 @@ export default {
           this.appointment.nightEntrance = '18:00'
         }
         this.appointment.nightOut = time
-      } else if (button === 'particularExit') {
-        if (!this.appointment.particularExit) {
-          this.appointment.particularExit = time
-        } else {
-          this.appointment.particularExitReturn = time
-        }
+      } else if (field === 'Saída Partícular') {
+        this.appointment.particularExit = time
+      } else if (field === 'Retorno') {
+        this.appointment.particularExitReturn = time
       }
       this.verifyButtons()
       this.registerAppointments()
