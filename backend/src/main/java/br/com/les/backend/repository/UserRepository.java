c209d38e23550db5ce0190550ccec161eb2269d2
José Victor Zeller <josevictorzg@gmail.com>
2018-10-21 18:45:40 -0300

    Adicona metodo para carregar usuarios por email

--- backend/src/main/java/br/com/les/backend/repository/UserRepository.java
+++ backend/src/main/java/br/com/les/backend/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package br.com.les.backend.repository;
 
+import org.springframework.data.jpa.repository.Query;
+
 import br.com.les.backend.entity.User;
 
 public interface UserRepository extends GenericRepository<User> {
 
 	<T> T findByEmailAndPassword(String email, String password);
+	<T> T findByEmail(String email);
 
 }
 
