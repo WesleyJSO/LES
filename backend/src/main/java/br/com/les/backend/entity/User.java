766dda08e9c03185019aefddbcd57b5c7f83d671
José Victor Zeller <josevictorzg@gmail.com>
2018-10-21 18:21:08 -0300

    Adiciona métodos de validação para controle de acesso

--- backend/src/main/java/br/com/les/backend/entity/User.java
+++ backend/src/main/java/br/com/les/backend/entity/User.java
@@ -11,36 +11,40 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Component
 @Entity
-public class User extends DomainEntity {
+@SuppressWarnings("serial")
+public class User extends DomainEntity implements UserDetailsService {
 
 	private String email;
 	private String password;
 	private String name;
 	private String lastName;
-	
-	@ManyToMany(cascade=CascadeType.ALL)
-    @JoinTable(
-    	name="USER_ROLE", 
-        joinColumns={@JoinColumn(name="USER_ID")}, 
-        inverseJoinColumns={@JoinColumn(name="ROLE_ID")}
-    )
-	private List< Role > roleList;
-	
-	@OneToMany( mappedBy="user", cascade=CascadeType.ALL, orphanRemoval=true )
-	@LazyCollection(LazyCollectionOption.FALSE)
-	private List< LogAction > logActionList;
-	
-	@OneToMany( mappedBy="user", cascade=CascadeType.ALL, orphanRemoval=true )
-	@JsonManagedReference
-	private List< Telephone > telephoneList;
 
-	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "USER_ROLE", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = {
+			@JoinColumn(name = "ROLE_ID") })
+	@JsonProperty("authorities")
+	private List<Role> roleList;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	@LazyCollection(LazyCollectionOption.FALSE)
+	private List<LogAction> logActionList;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonManagedReference
+	private List<Telephone> telephoneList;
+
 	public String getEmail() {
 		return email;
 	}
@@ -48,7 +52,7 @@ public class User extends DomainEntity {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public List<Role> getRoleList() {
 		return roleList;
 	}
@@ -65,18 +69,18 @@ public class User extends DomainEntity {
 		this.logActionList = logActionList;
 	}
 
-	public List< Telephone > getTelephoneList() {
+	public List<Telephone> getTelephoneList() {
 		return telephoneList;
 	}
 
-	public void setTelephoneList(List< Telephone > telephoneList) {
+	public void setTelephoneList(List<Telephone> telephoneList) {
 		this.telephoneList = telephoneList;
 	}
 
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
@@ -95,5 +99,15 @@ public class User extends DomainEntity {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserBuilder builder = null;
+		builder = org.springframework.security.core.userdetails.User.withUsername(username);
+		builder.password(new BCryptPasswordEncoder().encode(this.password));
+		builder.roles("ROLE_USER");
+
+		return builder.build();
 	}
 }
