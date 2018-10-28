385fd8f1120204e604dc7efaa411df866f3b9340
José Victor Zeller <josevictorzg@gmail.com>
2018-10-28 16:06:55 -0300

    Adds password encrypter

--- backend/src/main/java/br/com/les/backend/BackendApplication.java
+++ backend/src/main/java/br/com/les/backend/BackendApplication.java
@@ -2,11 +2,19 @@ package br.com.les.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main( String[] args ) {
 		SpringApplication.run( BackendApplication.class, args );
+	}
+	
+	// Make password encrypt an autowirable Been for all application
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
