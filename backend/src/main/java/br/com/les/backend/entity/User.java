34e25d6f22c4cc6ac58639ce9a8261b9f6d34d20
José Victor Zeller <josevictorzg@gmail.com>
2018-10-28 17:04:14 -0300

    Add security implementation properties on 'User' and 'Role' entity.

--- backend/src/main/java/br/com/les/backend/entity/User.java
+++ backend/src/main/java/br/com/les/backend/entity/User.java
@@ -1,5 +1,6 @@
 package br.com.les.backend.entity;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -7,24 +8,23 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Component
 @Entity
-public class User extends DomainEntity implements UserDetailsService {
+public class User extends DomainEntity implements UserDetails {
 
 	private String email;
 	private String password;
+	private boolean accountNonExpired;
+	private boolean accountNonLocked;
+	private boolean credentialsNonExpired;
+	private boolean enabled;
 	
 	@ManyToMany(cascade={ CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE })
 	@JoinTable(name = "USER_ROLE", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = {
@@ -41,10 +41,6 @@ public class User extends DomainEntity implements UserDetailsService {
 		this.email = email;
 	}
 
-	public List<Role> getRoleList() {
-		return roleList;
-	}
-
 	public void setRoleList(List<Role> roleList) {
 		this.roleList = roleList;
 	}
@@ -52,18 +48,56 @@ public class User extends DomainEntity implements UserDetailsService {
 	public String getPassword() {
 		return password;
 	}
-
+	
+	// Expected an encoded password 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		UserBuilder builder = null;
-		builder = org.springframework.security.core.userdetails.User.withUsername(username);
-		builder.password(new BCryptPasswordEncoder().encode(this.password));
-		builder.roles("ROLE_USER");
-
-		return builder.build();
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.roleList;
 	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return this.accountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return this.accountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return this.credentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
 }
