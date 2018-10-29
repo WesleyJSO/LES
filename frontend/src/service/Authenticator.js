cd24f9b7bd0d223a3b6581d01fd06d7502496a7b
José Victor Zeller <josevictorzg@gmail.com>
2018-10-29 00:17:53 -0300

    Fix vue security implementaion

--- frontend/src/service/Authenticator.js
+++ frontend/src/service/Authenticator.js
@@ -1,10 +1,12 @@
 export default class Authenticator {
   static HAS_ROLE (role) {
-    if (localStorage.principal) {
-      let principal = JSON.parse(localStorage.principal)
+    let principal = JSON.parse(sessionStorage.getItem('principal'))
+    if (principal) {
       return principal.authorities.find(i => i.authority === role)
+    } else {
+      console.log('#### There is NO User ####')
+      console.log('#### Redirecting To Login ####')
+      return false
     }
-    console.log('#### Unauthorized User ####')
-    return false
   }
 }
