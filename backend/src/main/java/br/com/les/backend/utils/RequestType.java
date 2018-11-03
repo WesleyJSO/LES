69ea30ad07d3ca717b60f440a0d623d6e677d229
José Victor Zeller <josevictorzg@gmail.com>
2018-11-03 19:43:50 -0300

    Adjust types of 'Requests'

--- backend/src/main/java/br/com/les/backend/utils/RequestType.java
+++ backend/src/main/java/br/com/les/backend/utils/RequestType.java
@@ -1,9 +1,9 @@
 package br.com.les.backend.utils;
 
 public class RequestType {
-	public static final int CHANGE_APPOINTMENT = 1;
-	public static final int WORK_OVERTIME = 2;
-	public static final int COMP_TIME = 3;
+	public static final int WORK_OVERTIME = 1;
+	public static final int COMP_TIME = 2;
+	public static final int CHANGE_APPOINTMENT = 3;
 	public static final int REALOCATION_DAYS = 4;
 	
 	private int value;
